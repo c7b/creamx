38919b540408345136d25bdbae6da09f99f2a05a
c7b <leckam7@gmail.com>
2023-05-17 12:58:51 -0600

    fix header mobile

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,12 +2,12 @@ function Header() {
 
 
     return(
-        <>
-        <h1 className="font-black text-center text-5xl w-1/2 mx-auto md:w-2/3">
-            Seguimiento Pacientes{' '}
-            <span className="text-indigo-600">Veterinaria</span>
-        </h1>
-        </>
+            <>
+                <h1 className="font-black text-center text-5xl w-full md:w-1/2 lg:w-2/3 mx-auto">
+                    Seguimiento Pacientes{' '}
+                    <span className="text-indigo-600">Veterinaria</span>
+                </h1>
+            </>
         )
 }
 
